dfcc9d84de316aab5a278bc224301c460c190b7b
tkk00 <takaki.o.h12@gmail.com>
2026-01-20 21:59:36 +0900

    Remove redundant parameter from layer activation

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2022 The ZMK Contributors
  *
@@ -560,13 +561,13 @@ static bool automouse_triggered = false;
 
 static void activate_automouse_layer() {
     automouse_triggered = true;
-    zmk_keymap_layer_activate(AUTOMOUSE_LAYER, false);
+    zmk_keymap_layer_activate(AUTOMOUSE_LAYER);
     k_timer_start(&automouse_layer_timer, K_MSEC(CONFIG_PMW3610_AUTOMOUSE_TIMEOUT_MS), K_NO_WAIT);
 }
 
 static void deactivate_automouse_layer(struct k_timer *timer) {
     automouse_triggered = false;
-    zmk_keymap_layer_deactivate(AUTOMOUSE_LAYER, false);
+    zmk_keymap_layer_deactivate(AUTOMOUSE_LAYER);
 }
 
 K_TIMER_DEFINE(automouse_layer_timer, deactivate_automouse_layer, NULL);
